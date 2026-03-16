3c60292a8af39ef5e28e325e67bad5fcf82000f6
Malachi-Rowe <c28malachi.rowe@afacademy.af.edu>
2026-03-16 08:05:28 -0600

    Updated right turn again

--- src/thunderbird_fsm.vhd
+++ src/thunderbird_fsm.vhd
@@ -132,9 +132,10 @@ begin
 	o_lights_L(0) <= f_Q(2) or f_Q(1) or f_Q(0) or f_Q(6);
 	o_lights_L(1) <= f_Q(1) or f_Q(0) or f_Q(6);
 	o_lights_L(2) <= f_Q(0) or f_Q(6);
-	o_lights_R(0) <= f_Q(3) or f_Q(6);
-	o_lights_R(1) <= f_Q(4) or f_Q(3) or f_Q(6);
 	o_lights_R(2) <= f_Q(5) or f_Q(4) or f_Q(3) or f_Q(6);
+	o_lights_R(1) <= f_Q(4) or f_Q(3) or f_Q(6);
+	o_lights_R(0) <= f_Q(3) or f_Q(6);
+	
 	
     ---------------------------------------------------------------------------------
 	
